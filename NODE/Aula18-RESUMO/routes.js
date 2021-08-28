264e064dffded40419cf6d3120aa7c8e2b44a46c
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-08-28 02:01:19 -0300

    return in project agenda!

--- NODE/Aula18-RESUMO/routes.js
+++ NODE/Aula18-RESUMO/routes.js
@@ -3,14 +3,6 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-// function meuMiddleware(req, res, next) {
-//   req.session = { nome: 'Rafael', senha: 'qwe@123' };
-//   console.log();
-//   console.log('Passei pelo Middleware');
-//   console.log();
-//   next();
-// }
-
 //ROTAS PAGINA INICIAL
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataForm);
@@ -19,3 +11,4 @@ route.post('/', homeController.trataForm);
 route.get('/contato', contatoController.paginaInicial);
 
 module.exports = route;
+
