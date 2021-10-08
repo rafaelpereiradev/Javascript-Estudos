ce11425dc550bc367d589727d2a019d902efe85a
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-10-08 12:22:11 -0300

    criando a tela de login

--- NODE/agenda/frontend/main.js
+++ NODE/agenda/frontend/main.js
@@ -3,4 +3,6 @@ import 'regenerator-runtime/runtime';
 
 import './assets/css/style.css';
 
-console.log('Olá mundo 3');
+console.log(
+  'Olá mundo, ele log vem do arquivo ./frontend/main.js configurado com webpack =)'
+);
