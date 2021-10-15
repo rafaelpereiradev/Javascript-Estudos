21efd63a23d564162cb4f6901e2b37b8b65b6d50
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-10-15 01:22:03 -0300

    organizado os arquivos git

--- NODE/agenda/frontend/main.js
+++ NODE/agenda/frontend/main.js
@@ -2,7 +2,3 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import './assets/css/style.css';
-
-console.log(
-  'Olá mundo, ele log vem do arquivo ./frontend/main.js configurado com webpack =)'
-);
