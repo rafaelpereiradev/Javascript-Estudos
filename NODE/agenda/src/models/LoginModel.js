45b17f0b0e11b35159442145012fb02c53d8d1b1
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-10-21 12:36:05 -0300

    alterando flash messages para tratar as mensagens de sucesso e erros

--- NODE/agenda/src/models/LoginModel.js
+++ NODE/agenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -17,11 +18,20 @@ class Login {
   async register() {
     this.valida();
     if (this.errors.length > 0) return;
+
+    await this.userExists();
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
     try{
-    this.user = await LoginModel.create(this.body)
-    }catch(erro){
+     
+     }catch(erro){
       console.log(erro);
     }
+  }
+  async userExists() {
+   const user =  await LoginModel.findOne({email: this.body.email});
+   if(user) this.errors.push('Usuario ja existe')
   }
   valida() {
     //Validação
